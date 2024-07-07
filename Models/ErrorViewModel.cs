941778473e04ab0fc749870aa12c15f344e9dad2
GajendraChoudhary <gajendrachoudhary18@gmail.com>
2024-07-07 13:20:34 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CSV_to_Database.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
