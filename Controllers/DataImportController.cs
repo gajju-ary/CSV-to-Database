70bef461b0ba000e6aed4b5acef3f4f16f613be5
GajendraChoudhary <gajendrachoudhary18@gmail.com>
2024-07-12 00:24:55 +0530

    HomePage ChangesCreated New Controller for Data Import

---
+++ Controllers/DataImportController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSV_to_Database.Controllers
+{
+    public class DataImportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ManageData()
+        {
+            return View();
+        }
+    }
+}
